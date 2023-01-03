8370b91ba3b2d5b6299c6ce23ee82537d07ae2fd
Fireball19 <fireball1199@googlemail.com>
2023-01-03 23:40:05 +0100

    Add long variables

---
+++ Packages/Variables/Runtime/Immutable/LongImmutableVariable.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace RaCoding.Variables
+{
+    [CreateAssetMenu(fileName = "LongImmutableVariable", menuName = "RaCoding/Variables/Immutable/Create new immutable long variable")]
+    public class LongImmutableVariable : ImmutableVariable<long> {}
+}
