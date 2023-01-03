7734f5a3721b7534fd42039c0052f3a1dfe88bd3
Fireball19 <fireball1199@googlemail.com>
2023-01-03 23:39:56 +0100

    Add char variables

---
+++ Packages/Variables/Runtime/Mutable/CharMutableVariable.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace RaCoding.Variables
+{
+    [CreateAssetMenu(fileName = "CharMutableVariable", menuName = "RaCoding/Variables/Mutable/Create new mutable char variable")]
+    public class CharMutableVariable : MutableVariable<char> {}
+}
