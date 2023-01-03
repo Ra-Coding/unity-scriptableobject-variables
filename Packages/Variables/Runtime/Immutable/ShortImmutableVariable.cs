20fd17ade9cee23cdb1d57d43e29622d34d559dc
Fireball19 <fireball1199@googlemail.com>
2023-01-03 23:40:15 +0100

    Add short variables

---
+++ Packages/Variables/Runtime/Immutable/ShortImmutableVariable.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace RaCoding.Variables
+{
+    [CreateAssetMenu(fileName = "ShortImmutableVariable", menuName = "RaCoding/Variables/Immutable/Create new immutable short variable")]
+    public class ShortImmutableVariable : ImmutableVariable<short> {}
+}
