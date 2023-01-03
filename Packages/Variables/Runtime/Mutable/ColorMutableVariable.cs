96aee9ec44c56b5adb495a69f18726aec77e1183
Fireball19 <fireball1199@googlemail.com>
2023-01-03 23:44:31 +0100

    Add color variables

---
+++ Packages/Variables/Runtime/Mutable/ColorMutableVariable.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace RaCoding.Variables
+{
+    [CreateAssetMenu(fileName = "ColorMutableVariable", menuName = "RaCoding/Variables/Mutable/Create new mutable color variable")]
+    public class ColorMutableVariable : MutableVariable<Color> {}
+}
