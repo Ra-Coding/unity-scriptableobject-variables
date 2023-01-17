60455a6d6def615857863a75aaa71ee03b6b6b6e
Fireball19 <fireball1199@googlemail.com>
2023-01-17 21:15:39 +0100

    Add vector3int variables

---
+++ Packages/SOVariables/Runtime/Mutable/Vector3IntMutableVariable.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace RaCoding.Variables
+{
+    [CreateAssetMenu(fileName = "Vector3IntMutableVariable", menuName = "RaCoding/Variables/Mutable/Create new mutable vector3int variable")]
+    public class Vector3IntMutableVariable : MutableVariable<Vector3Int> { }
+}
