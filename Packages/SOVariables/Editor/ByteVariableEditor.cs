779e75fb332fff57cf28277c2880074d5f448a96
Fireball19 <fireball1199@googlemail.com>
2023-01-17 19:37:48 +0100

    Add byte variable editor script

---
+++ Packages/SOVariables/Editor/ByteVariableEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace RaCoding.Variables
+{
+    [CustomEditor(typeof(ByteMutableVariable), editorForChildClasses: true)]
+    public class ByteVariableEditor : VariableEditor<byte>
+    {
+        protected override void AssignResetValue()
+        {
+            resetValue.intValue = value.intValue;
+        }
+    }
+}
