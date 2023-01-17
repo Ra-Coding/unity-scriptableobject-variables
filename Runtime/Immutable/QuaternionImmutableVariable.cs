f014218288eaa14a997dd3de7d51fd3b36ecae95
Fireball19 <fireball1199@googlemail.com>
2023-01-17 21:21:26 +0100

    Add quaternion variables

---
+++ Runtime/Immutable/QuaternionImmutableVariable.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RaCoding.Variables
+{
+    public class QuaternionImmutableVariable : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
