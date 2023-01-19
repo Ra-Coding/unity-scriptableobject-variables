9eff8f89ce9563a05fb6dd9d7388f3329940ea3a
Fireball19 <fireball1199@googlemail.com>
2023-01-19 12:24:42 +0100

    Fix missing vector2 immutable variable code

--- Runtime/Immutable/Vector2ImmutableVariable.cs
+++ Runtime/Immutable/Vector2ImmutableVariable.cs
@@ -1,21 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace RaCoding.Variables
 {
-    public class Vector2ImmutableVariable : MonoBehaviour
-    {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
-    }
+    [CreateAssetMenu(fileName = "Vector2ImmutableVariable", menuName = "RaCoding/Variables/Immutable/Create new immutable vector2 variable")]
+    public class Vector2ImmutableVariable : ImmutableVariable<Vector2> { }
 }
