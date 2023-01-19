6afc45316d94fa1e89ba43a6d99dde0c16e0dd52
Fireball19 <fireball1199@googlemail.com>
2023-01-19 12:15:03 +0100

    Add ushort variables

---
+++ Runtime/Immutable/UShortImmutableVariable.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace RaCoding.Variables
+{
+    [CreateAssetMenu(fileName = "UShortImmutableVariable", menuName = "RaCoding/Variables/Immutable/Create new immutable ushort variable")]
+    public class UShortImmutableVariable : ImmutableVariable<ushort> { }
+}
---
+++ Runtime/Mutable/UShortMutableVariable.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace RaCoding.Variables
+{
+    [CreateAssetMenu(fileName = "UShortMutableVariable", menuName = "RaCoding/Variables/Mutable/Create new mutable ushort variable")]
+    public class UShortMutableVariable : MutableVariable<ushort> { }
+}
