dd39170ab2c105c580eebd09dcf1d6c1f6e80764
Fireball19 <fireball1199@googlemail.com>
2023-01-19 12:04:47 +0100

    Add double variables

---
+++ Packages/SOVariables/Runtime/Mutable/DoubleMutableVariable.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace RaCoding.Variables
+{
+    [CreateAssetMenu(fileName = "DoubleMutableVariable", menuName = "RaCoding/Variables/Mutable/Create new mutable double variable")]
+    public class DoubleMutableVariable : MutableVariable<double> { }
+}
