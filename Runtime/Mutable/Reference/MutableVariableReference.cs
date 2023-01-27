f272bb638c3c01e8316f3a1ed08bd0232c712971
Fireball19 <fireball1199@googlemail.com>
2023-01-27 18:39:32 +0100

    Add mutable variable reference

---
+++ Runtime/Mutable/Reference/MutableVariableReference.cs
@@ -0,0 +1,15 @@
+namespace RaCoding.Variables
+{
+    public abstract class MutableVariableReference<T> : IVariable<T>
+    {
+        protected abstract MutableVariable<T> Reference { get; }
+        
+        public T Value
+        {
+            get 
+            {
+                return Reference.Value;
+            }
+        }
+    }
+}
